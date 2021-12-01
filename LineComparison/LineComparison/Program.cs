abd8a2b61d69316597c53360e4765a21d3e0eaf2
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-01 19:01:33 +0530

    Compare two Lines

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -3,11 +3,14 @@ UC1_Length lc = new UC1_Length();
 
 //uc2
 UC2_EqualityOFLine len1 = new UC2_EqualityOFLine();
+//u3
+UC3_CompareLine uc3 = new UC3_CompareLine();
 
 bool Option = true;
 while(Option)
 {
-    Console.Write("Select UC Number:\n1.Check Lenght of Line\n2.Check Equality of Two Lines\n");
+    Console.Write("Select UC Number:\n1.Check Lenght of Line\n2.Check Equality of Two Lines\n" +
+        "3.Compare Two Lines\n\n");
     int checkoption = Convert.ToInt32(Console.ReadLine());
     switch(checkoption)
      {
@@ -17,6 +20,9 @@ while(Option)
         case 2:
             len1.checkequal();
           
+            break;
+        case 3:
+            uc3.compare();
             break;
 
 
